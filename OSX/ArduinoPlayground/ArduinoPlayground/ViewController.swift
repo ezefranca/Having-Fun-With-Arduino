88a8fe1f7e23b9bb4ad4d77775574db3c01c6c85
kviksilver <berceg@gmail.com>
2014-10-29 19:06:01 -0400

    bindings?

--- OSX/ArduinoPlayground/ArduinoPlayground/ViewController.swift
+++ OSX/ArduinoPlayground/ArduinoPlayground/ViewController.swift
@@ -10,15 +10,47 @@ import Cocoa
 
 
 
-class ViewController: NSViewController {
+class ViewController: NSViewController , ORSSerialPortDelegate{
 
     @IBOutlet var serialPortSelector: NSPopUpButton!
     @IBOutlet var arrayController: NSArrayController!
     @IBOutlet var selectedObject: ORSSerialPort!
     
+    @IBOutlet var infoLabel: NSTextField!
+    @IBOutlet var baudRateSelector: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         arrayController.content = ORSSerialPortManager.sharedSerialPortManager().availablePorts
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        selectedObject.delegate = self;
+        if let baud = baudRateSelector.stringValue.toInt() {
+            selectedObject.baudRate = baud
+        }
+        selectedObject.open();
+        
+    }
+    
+    func serialPort(serialPort: ORSSerialPort!, didEncounterError error: NSError!) {
+        
+    }
+    
+    func serialPort(serialPort: ORSSerialPort!, didReceiveData data: NSData!) {
+        var receivedString = NSString(data: data, encoding: NSUTF8StringEncoding)
+        infoLabel.stringValue = receivedString as String
+    }
+    
+    func serialPortWasClosed(serialPort: ORSSerialPort!) {
+        
+    }
+    
+    func serialPortWasOpened(serialPort: ORSSerialPort!) {
+        
+    }
+    
+    func serialPortWasRemovedFromSystem(serialPort: ORSSerialPort!) {
+        
+    }
 }
